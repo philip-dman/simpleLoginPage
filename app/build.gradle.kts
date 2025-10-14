d8a96e6d47bf863188b77a7e79b35ad26d5574c0
philip-dman <hanzphilip.manaog@email.lcup.edu.ph>
2025-10-14 19:18:18 +0800

    login design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
 
+    implementation("com.google.android.material:material:1.9.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
